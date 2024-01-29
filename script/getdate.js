0476030086cc51113cb69f694b83b38640e9b68f
emmanuelologe <epicraez@gmail.com>
2024-01-29 03:12:58 +0100

    Week 2 Update

--- script/getdate.js
+++ script/getdate.js
@@ -6,4 +6,27 @@ document.addEventListener('DOMContentLoaded', function() {
     const lastModifiedParagraph = document.getElementById('lastModified');
     const lastModified = new Date(document.lastModified);
     lastModifiedParagraph.textContent = `Last modified: ${lastModified.toLocaleString()}`;
+
+    const menuToggle = document.getElementById('menu');
+    const nav = document.querySelector('nav');
+  
+    menuToggle.addEventListener('click', () => {
+      nav.classList.toggle('show');
+    });
+
+    const modeButton = document.querySelector("#mode");
+    const main = document.querySelector("main");
+    
+    modeButton.addEventListener("click", () => {
+    	if (modeButton.textContent.includes("🕶️")) {
+    		main.style.background = "#000";
+    		main.style.color = "#fff";
+    		modeButton.textContent = "🔆";
+    	} else {
+    		main.style.background = "#eee";
+    		main.style.color = "#000";
+    		modeButton.textContent = "🕶️";
+    	}
+    });
+
 });
